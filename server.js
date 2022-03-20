4b86db193f9b3bd89758a6e05ede5d62504011e4
unknown <prometheus.lazo@telehealth.ph>
2022-03-20 16:17:26 +0800

    test auth

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const jwt = require("jsonwebtoken");
+
 app.use(express.static(__dirname + '/dist/pizzalasso-demo'));
+
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/pizzalasso-demo/index.html'));
 });
-app.post("/test", async (req, res) => {
-    res.status(201).json({test: "test"});
+
+app.post("/auth", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!(username && password)) {
+            res.status(400).send("All input is required");
+        }
+        const user = { _id: "1", username: "user", password: "password" };    
+        if (user && password === user.password) {          
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                // process.env.TOKEN_KEY,
+                "TOKEN_KEY",
+                { expiresIn: "2h", }
+            );    
+            user.token = token;    
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
 });
 app.listen(process.env.PORT || 8080);
