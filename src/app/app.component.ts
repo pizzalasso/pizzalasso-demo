bb584df58bc493f1f9cad67e3526c3b0ffd27979
unknown <prometheus.lazo@telehealth.ph>
2022-03-20 20:03:12 +0800

    sign out handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as CustomEditor from '.ckeditor';
-import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
-import { UploadAdapter } from './shared/upload-adapter';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,38 +8,47 @@ import { UploadAdapter } from './shared/upload-adapter';
 })
 export class AppComponent implements OnInit{
   
-  title = 'pizzalasso-demo';
+  // title = 'pizzalasso-demo';
 
   authenticated = localStorage.getItem('token_data');
 
-  public config: any;
+  // public config: any;
 
-  editorData = '';
+  // editorData = '';
 
-  public Editor = CustomEditor;  
-  public model = {
-    editorData: this.editorData
-  };
+  // public Editor = CustomEditor;  
+  // public model = {
+  //   editorData: this.editorData
+  // };
 
-  public Editor2 = CustomEditor;  
-  public model2 = {
-    editorData: this.editorData
-  };
+  // public Editor2 = CustomEditor;  
+  // public model2 = {
+  //   editorData: this.editorData
+  // };
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  public onChange( { editor }: ChangeEvent ) {
-    const data = editor.getData();
-    console.log( data );
-    this.model2.editorData = data;
-  }
+  // public onChange( { editor }: ChangeEvent ) {
+  //   const data = editor.getData();
+  //   console.log( data );
+  //   this.model2.editorData = data;
+  // }
 
-  onReady(eventData: any) {
-    eventData.plugins.get('FileRepository').createUploadAdapter = function (loader: any) {
-      console.log(btoa(loader.file));
-      return new UploadAdapter(loader);
-    };
+  // onReady(eventData: any) {
+  //   eventData.plugins.get('FileRepository').createUploadAdapter = function (loader: any) {
+  //     console.log(btoa(loader.file));
+  //     return new UploadAdapter(loader);
+  //   };
+  // }
+
+  signOut() {
+    localStorage.clear();
+    this.router.navigateByUrl('/');
   }
 
 }
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { first } from 'rxjs';
 import { DemoService } from '../demo.service';
 
@@ -18,7 +18,6 @@ export class SignInComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
     private router: Router,
     private demoService: DemoService
   ) { 
