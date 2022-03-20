8ed19e70d10f3bd4030a9c854d9911c619b747e0
unknown <prometheus.lazo@telehealth.ph>
2022-03-20 16:07:23 +0800

    test post

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ app.use(express.static(__dirname + '/dist/pizzalasso-demo'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/pizzalasso-demo/index.html'));
 });
+app.post("/test", async (req, res) => {
+    res.status(201).json({test: "test"});
+});
 app.listen(process.env.PORT || 8080);
